25fa2bca987005441a7e95d64742d75e676d9012
marlleyck <marlleyck@hotmail.com>
2022-11-26 10:20:03 -0300

    style: item created

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
+import { AddTaskArea } from '../../components/AddTaskArea';
+import { ListItems } from '../../components/ListItems';
+
 import { Container } from './styles';
 
 export const Home = () => {
     return  (
         <Container>
-            
+            <AddTaskArea />
+            <ListItems />
         </Container>
     );
 }
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,3 +1,13 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: column;
+
+    background-color: #242424;
+`
