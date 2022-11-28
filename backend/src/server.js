cf934f9c1cbd6d4d86916b367390a97160d29583
marlleyck <marlleyck@hotmail.com>
2022-11-28 10:41:04 -0300

    feat: cors installed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+
 const authController = require('./controllers/authController')
 const connection = require('./database')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
