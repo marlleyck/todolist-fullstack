5d414682b9328bcc74d901cd231d10e9725478fa
marlleyck <marlleyck@hotmail.com>
2023-05-09 17:52:07 -0300

    feat: change endpoints response

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,88 +1,94 @@
-const express = require('express')
-const Task = require('../models/Task')
+const express = require("express");
+const Task = require("../models/Task");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/auth/register', async (req, res) => {
-    const { title, description, completed } = req.body
+router.post("/auth/register", async (req, res) => {
+  const { title, description, completed } = req.body;
 
-    if (!title) {
-        return res.status(400).send({ error: 'Title required!' })
+  if (!title) {
+    return res.status(400).send({ error: "Title required!" });
+  }
+
+  if (completed === undefined) {
+    return res.status(400).send({ error: "Completed required!" });
+  }
+
+  const newTask = await Task.create({
+    title: title,
+    description: description,
+    completed: completed,
+  });
+
+  const tasks = await Task.findAll();
+
+  return res.status(201).send({ tasks, msg: "Task created!" });
+});
+
+router.get("/tasks", async (req, res) => {
+  const tasks = await Task.findAll();
+
+  return res.status(200).send({ tasks });
+});
+
+router.get("/task/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).send({ error: "Id required!" });
+  }
+
+  const task = await Task.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!task) {
+    return res.status(404).send({ error: "Task not found!" });
+  }
+
+  return res.status(200).send({ task });
+});
+
+router.put("/task/:id", async (req, res) => {
+  const { title, description, completed } = req.body;
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).send({ error: "Id required!" });
+  }
+
+  await Task.update(
+    { title: title, description: description, completed: completed },
+    {
+      where: {
+        id: id,
+      },
     }
+  );
 
-   if (completed === undefined) {
-        return res.status(400).send({ error: 'Completed required!' })
-   }
+  const tasks = await Task.findAll();
 
-    const newTask = await Task.create({
-        title: title,
-        description: description,
-        completed: completed
-    })
+  return res.status(200).send({ tasks, msg: "Updated task successfully!" });
+});
 
-    return res.status(201).send({ msg: 'Task created!' })
-})
+router.delete("/task/:id", async (req, res) => {
+  const { id } = req.params;
 
-router.get('/tasks', async (req, res) => {
-    const tasks = await Task.findAll()
+  if (!id) {
+    return res.status(400).send({ error: "Id required!" });
+  }
 
-    return res.status(200).send({ tasks })
-})
+  await Task.destroy({
+    where: {
+      id: id,
+    },
+  });
 
-router.get('/task/:id', async (req, res) => {
-    const { id } = req.params
+  const tasks = await Task.findAll();
 
-    if (!id) {
-        return res.status(400).send({ error: 'Id required!' })
-    }
+  return res.status(200).send({ tasks, msg: "Deleted success!" });
+});
 
-    const task = await Task.findOne({
-        where: {
-            id: id
-        }
-    })
-
-    if (!task) {
-        return res.status(404).send({ error: 'Task not found!' })
-    }
-
-    return res.status(200).send({ task })
-})
-
-router.put('/task/:id', async (req, res) => {
-    const { title, description, completed } = req.body
-    const { id } = req.params
-
-    if (!id) {
-        return res.status(400).send({ error: 'Id required!' })
-    }
-
-    await Task.update(
-        { title: title, description: description, completed: completed },
-        {
-            where: {
-                id: id
-            }
-        }
-    )
-
-    return res.status(200).send('Updated task successfully!')
-})
-
-router.delete('/task/:id', async (req, res) => {
-    const { id } = req.params
-
-    if (!id) {
-        return res.status(400).send({ error: 'Id required!' })
-    }
-    
-    await Task.destroy({
-        where: {
-            id: id
-        }
-    })
-
-    return res.status(200).send({ msg: 'Deleted success!' })
-})
-
-module.exports = router
+module.exports = router;
